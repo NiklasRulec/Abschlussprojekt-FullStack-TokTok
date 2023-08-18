d752460b8b62803ca2f77f7e6ef6046b84bb5595
NiklasRulec <niklasrulec.98@gmail.com>
2023-08-18 11:52:36 +0200

    upload

--- frontend/src/components/InfoBar/InfoBar.jsx
+++ frontend/src/components/InfoBar/InfoBar.jsx
@@ -2,12 +2,22 @@ import "./InfoBar.css";
 import Empfang from "../../images/Empfang.png";
 import Wifi from "../../images/Wifi.png";
 import Accu from "../../images/Accu.png";
+import { useEffect, useState } from "react";
 
 const InfoBar = () => {
+  const [time, setTime] = useState();
+
+  useEffect(() => {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    setTime(`${hours}:${minutes}`);
+  }, []);
+
   return (
     <>
       <div className="time">
-        <h6>09:41</h6>
+        <h6>{time}</h6>
       </div>
       <div className="top-bar">
         <img src={Empfang} alt="empfang" className="reception" />
