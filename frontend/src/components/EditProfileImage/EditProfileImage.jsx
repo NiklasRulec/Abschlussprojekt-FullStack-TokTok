50fe167699eda316b1d8093ab056cfea6015e7ed
NiklasRulec <niklasrulec.98@gmail.com>
2023-08-24 11:46:28 +0200

    edit-btn-img

--- frontend/src/components/EditProfileImage/EditProfileImage.jsx
+++ frontend/src/components/EditProfileImage/EditProfileImage.jsx
@@ -2,7 +2,7 @@ import "./EditProfileImage.css";
 import React, { useContext, useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import edit from "../../images/EditSquare.svg";
+import edit from "../../images/edit-img.svg";
 import { RefreshContext } from "../../user/RefreshContext";
 import Avatar from "../../images/Avatar.svg";
 
